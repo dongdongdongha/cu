83bb3f6720b32a64d24b1d431aaee767d8c8bf81
dongdongdongha <109192056+dongdongdongha@users.noreply.github.com>
2023-04-05 11:08:04 +0900

    Add files via upload

---
+++ ASDASFDS.java
@@ -0,0 +1,64 @@
+package vfdbhnlfxknb;
+
+//class Student{
+//	String name;
+//	int grade;
+//	int ban;
+//	int number;
+//	String trlrphone;
+//}
+//
+//public class ASDASFDS {
+//	public static void main(String[] args) {
+//		Student kim = new Student();
+//		kim.name = "김현우";
+//		kim.grade = 2;
+//		System.out.println("이름 : "+ kim.name + ", 학년 : "+ kim.grade );
+//	}
+//}
+
+//class Student{
+//public String name;
+//int grade;
+//int ban;
+//int number;
+//String telephone;
+//}
+
+//public class ASDASFDS{
+//	public static void main(String[] args) {
+//		Student kim = new Student();
+//		kim.name = "김현우";
+//		kim.grade = 2;
+//		kim.telephone = "111-1111";
+//		System.out.println("이름 : "+ kim.name + ", 학년 : "+ kim.grade + ", 전번 : "+ kim.telephone);
+//	}
+//}
+
+class Student{
+	static String school = "hangang";
+	String name;
+	int grade;
+}
+
+public class ASDASFDS{
+	public static void main(String[] args) {
+		Student kim = new Student();
+		Student jang = new Student();
+		Student bak = new Student();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
