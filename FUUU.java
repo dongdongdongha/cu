6be63cd06030a680b7ba83a3e2046dba5df39b0f
dongdongdongha <109192056+dongdongdongha@users.noreply.github.com>
2023-04-05 10:29:36 +0900

    Add files via upload

---
+++ FUUU.java
@@ -0,0 +1,48 @@
+package vfdbhnlfxknb;
+
+public class FUUU {
+	public static void main(String[] args) {
+//		int i = 1;
+//		while(i <= 5) {
+//			System.out.print(i + "\t");
+//			i++;
+//		}
+		
+//		int i = 1, sum = 0;
+//		System.out.print("1-2+3-4+5-6+7-8+9-10=");
+//		while(i <= 10) {
+//			if(i % 2 == 0)
+//				sum -= i;
+//			else
+//				sum += i;
+//			i++;
+//		}
+//		System.out.print(sum);
+		
+//		int i = 5;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}while(i>10);
+		
+//		for(int i = 1; i <= 10; i++) {
+//			if(i == 5)
+//				break;
+//			System.out.print(i + "\t");
+//		}
+		
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.print(i + "\t");
+//			if(i == 5)
+//				break;
+//		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i == 5)
+				continue;
+			System.out.print(i + "\t");
+		}
+		
+		
+	}
+}
